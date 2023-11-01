ca8ad4097b855bf1859a4928d11c8b2fc964c1e2
Jamie Boyd <jadobo@gmail.com>
2023-10-31 22:35:31 -0700

    first commit

---
+++ GravityDOsensorApp/MeadowApp.cs
@@ -0,0 +1,55 @@
+﻿using Meadow;
+using Meadow.Devices;
+using GravityDO;
+using System;
+using System.Threading.Tasks;
+using Meadow.Units;
+
+namespace DOsensor_Sample
+{
+    // Change F7FeatherV2 to F7FeatherV1 for V1.x boards  
+    // Use F7CoreComputeV2 for ProjectLab V3
+    public class MeadowApp : App<F7CoreComputeV2>
+    {
+        //<!=SNIP=>
+
+        private GravityDOsensor sensor;
+
+        public override Task Initialize()
+        {
+            Console.WriteLine("Initialize...");
+
+            sensor = new GravityDOsensor(pin: Device.Pins.A04, sampleCount: 100, sampleInterval: TimeSpan.FromMilliseconds(10), voltage: new Voltage(3.3));
+
+            var consumer = GravityDOsensor.CreateObserver(
+                handler: result => Console.WriteLine($"Observer filter satisfied: {result.New.Millivolts:N2}mV, old: {result.Old?.Millivolts:N2}mV"),
+                filter: result =>
+                {
+                    if (result.Old is { } old)
+                    { //c# 8 pattern match syntax. checks for !null and assigns var.
+                        return (result.New - old).Abs().Millivolts > 500;
+                    }
+                    return false;
+                });
+            sensor.Subscribe(consumer);
+
+            // classical .NET events can also be used:
+            sensor.Updated += (sender, result) =>
+            {
+                Console.WriteLine($"Voltage Changed, new: {result.New.Millivolts:N2}mV, old: {result.Old?.Millivolts:N2}mV");
+            };
+
+            return Task.CompletedTask;
+        }
+
+        public override async Task Run()
+        {
+            var result = await sensor.Read();
+            Console.WriteLine($"Initial read: {result.Millivolts:N2}mV");
+
+            sensor.StartUpdating(TimeSpan.FromMilliseconds(1000));
+        }
+
+        //<!=SNOP=>
+    }
+}
---
+++ GravityDOsensorLib/GravityDOsensor.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Threading.Tasks;
+using Meadow;
+using Meadow.Foundation.Sensors.Base;
+using Meadow.Hardware;
+using Meadow.Peripherals.Sensors;
+using Meadow.Units;
+
+
+namespace GravityDO  // TODO:submit code to WildernessLabs and fit in their space
+{
+    /// <summary>
+    /// Represents an Atlas Scientific Gravity Dissolved Oxygen Meter with
+    /// analog voltage output
+    /// </summary>
+    public class GravityDOsensor : AnalogSamplingBase
+    {
+        // reference voltage from 100% saturated reading
+        private Voltage referenceVoltage;
+
+        /// <summary>
+        /// Creates a new GravityDOsensor object from existing analog input port
+        /// </summary>
+        /// <param name="port">an IAnalogInputPort.</param>
+        public GravityDOsensor(IAnalogInputPort port) : base(port)
+        {
+            this.referenceVoltage = new Voltage(-1);  // init to -1
+        }
+
+        /// <summary>
+        /// Creates a new GravityDOsensor object, creating the analog input port
+        /// from the given pin, with 10 samples, 10 ms sample interval, and
+        /// 3.3V reference voltage
+        /// </summary>
+        /// <param name="pin">AnalogChannel connected to the sensor.</param>
+        public GravityDOsensor(IPin pin) : base (pin.CreateAnalogInputPort(10,
+            TimeSpan.FromMilliseconds(10),
+            new Voltage(3.3)))
+        { }
+
+
+        public async setReference ()
+        {
+            referenceVoltage = await this.Read();
+        }
+        
+        public override TimeSpan UpdateInterval { get => base.UpdateInterval; protected set => base.UpdateInterval = value; }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override Task<Voltage> Read()
+        {
+            return base.Read();
+        }
+
+        public override void StartUpdating(TimeSpan? updateInterval)
+        {
+            base.StartUpdating(updateInterval);
+        }
+
+        public override void StopUpdating()
+        {
+            base.StopUpdating();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        protected override void RaiseEventsAndNotify(IChangeResult<Voltage> changeResult)
+        {
+            base.RaiseEventsAndNotify(changeResult);
+        }
+
+        protected override Task<Voltage> ReadSensor()
+        {
+            return base.ReadSensor();
+        }
+    }
+}
